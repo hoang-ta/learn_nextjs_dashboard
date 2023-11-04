c2eb9e1969a6b8b0ea71653f13caaf9807a2034b
Huy Hoang Ta <huyhoang.ta97@gmail.com>
2023-11-04 08:29:27 +0700

    update bg

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -32,7 +32,7 @@ export default function NavLinks() {
             href={link.href}
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              { "bg-gray-400 text-blue-400": pathname === link.href }
+              { "bg-gray-500 text-blue-400": pathname === link.href }
             )}
           >
             <LinkIcon className="w-6" />
